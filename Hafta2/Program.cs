7b2090dfbd5a189b077fb176887bdfc222088438
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-10-14 20:42:35 +0300

    C# Giriş

---
+++ Hafta2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Ogrenci 
+{
+    public String adi;
+    public int yasi;
+    public String sinifi;
+
+    public void BilgileriYazdir()
+    {
+        Console.WriteLine("Adi" + adi + " " + "Yasi:" + yasi.ToString() + " " + "Sinifi:" + sinifi);
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Ogrenci ogr1 = new Ogrenci();
+        ogr1.adi = "Ahmet";
+        ogr1.yasi = 20;
+        ogr1.sinifi = "10B";
+
+        ogr1.BilgileriYazdir();
+    }
+}
